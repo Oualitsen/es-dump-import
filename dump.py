42f6f92194c2ddfb94ab16238d7435a3e5ef842e
Ramdane Oualitsen <Ramdane>
2024-08-12 14:56:35 +0100

    first commit.

---
+++ dump.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+es_url = "http://localhost:9200"
+
+
+def dump(index):
+    print(f"dumping index {index}")
+    page_size = 100
+    page_index = 0
+    while True:
+        match_all = f'{{ "from": {page_index*page_size}, "size": {page_size}, "query": {{ "match_all": {{}} }} }}'
+        request_url = f"{es_url}/{index}/_search"
+        result = requests.post(request_url, data=match_all, headers={'Content-Type': 'application/json'}).json()
+        hits = result['hits']['hits']
+        final_result = [hit['_source'] for hit in hits]
+        if len(final_result) == 0:
+            break
+        # save these
+        with open(index, 'a', encoding="utf-8") as file:
+            for hit in final_result:
+                file.write(f"{json.dumps(hit)}\n" )
+        
+        page_index += 1
+        print(f"page = {page_index}")
+
+indicies = ["seasons", "products", "sku", "collection_mapping"]
+for index in indicies:
+    dump(index)
---
+++ import.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+es_url = "http://localhost:9200"
+
+
+def import_data(index, index_name):
+    print(f"importing index {index} ")
+    buffer_size = 100
+    buffer = []
+    page_index = 0
+    with open(index, encoding="utf-8") as file:
+        while True:
+            line = file.readline()
+            if len(line) > 0:
+                buffer.append(line)
+            else:
+                break
+            if len(buffer) == buffer_size:
+                print(f"index the buffer {page_index}")
+                page_index += 1
+                index_data(buffer[:], index_name)
+                buffer = []
+                
+        
+    if len(buffer) > 0:
+        print(f"index the the remaining buffer {len(buffer)}")
+        index_data(buffer[:], index_name)
+
+def index_data(data, index_name):
+    print(f"indexing {index_name}")
+    new_data = []
+    for element in data:
+        element_json = json.loads(element)
+        new_data.append({ "index": { "_id": element_json["id"] } })
+        del element_json['score']
+        new_data.append(element_json)
+    request_url = f"{es_url}/{index_name}/_bulk"
+    bulk_request = ""
+    for element in new_data:
+        bulk_request += f"{json.dumps(element)}\n"
+    requests.post(request_url, data=bulk_request, headers={'Content-Type': 'application/json'})
+
+indicies = ["seasons", "products", "sku", "collection_mapping"]
+
+for index in indicies:
+    import_data(index, f"{index}")
